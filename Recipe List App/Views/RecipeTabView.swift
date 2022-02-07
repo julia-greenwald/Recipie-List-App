327a81d690ad13309d00b6d50a585ae52305de9d
J.G. Greenwald <greenwald.juj@gmail.com>
2022-02-07 14:47:12 -0500

    Add RecipeTabView.

---
+++ Recipe List App/Views/RecipeTabView.swift
@@ -0,0 +1,35 @@
+//
+//  RecipeTabView.swift
+//  Recipe List App
+//
+//  Created by greenwald_juj on 2/7/22.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        TabView {
+            Text("Featured View")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
