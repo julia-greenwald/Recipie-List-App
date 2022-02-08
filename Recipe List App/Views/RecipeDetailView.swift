6771788aa25ca5331f39d328885ee6498fbfefbe
J.G. Greenwald <greenwald.juj@gmail.com>
2022-02-08 13:16:27 -0500

    Add Picker in RecipeDetailView.

--- Recipe List App/Views/RecipeDetailView.swift
+++ Recipe List App/Views/RecipeDetailView.swift
@@ -3,12 +3,15 @@
 //  Recipe List App
 //
 //  Created by greenwald_juj on 2/5/22.
-//
+
+
+// No reference to instance model, but recipe detail view is child of recipe list view. only need it to call getPortion.
 
 import SwiftUI
 
 struct RecipeDetailView: View {
     var recipe:Recipe // unset because we want the list view to determine which view is shown
+    @State var selectedServingSize = 2
     
     var body: some View {
         
@@ -21,15 +24,30 @@ struct RecipeDetailView: View {
                 Image(recipe.image)
                     .resizable()
                     .scaledToFill()
+                
+                // MARK: Select Picker
+                VStack {
+                    Picker("", selection: $selectedServingSize) {
+                        Text("2").tag(2)
+                        Text("4").tag(4)
+                        Text("6").tag(6)
+                        Text("8").tag(8)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .frame(width:160)
+                }
+                
                 // MARK: Ingredients
                 VStack(alignment: .leading) {
                     Text("Ingredients")
                         .font(.headline)
                         .padding([.bottom, .top] , 5)
+                    
                     ForEach (recipe.ingredients) { item in
-                        Text("• " + item.name)
+//                        Text("• " + item.name)
 //                    ForEach (recipe, id: \.self) { item in
 //                            Text("• " + item)
+                        Text("• " + RecipeModel.getPortion(ingredient: item, recipeServings: recipe.servings, targetServings: selectedServingSize) + " " + item.name.lowercased())
 
                     }
                 }
@@ -40,9 +58,11 @@ struct RecipeDetailView: View {
                 
                 // MARK: Directions
                 VStack(alignment: .leading){
+                    
                     Text("Directions")
                         .font(.headline)
                         .padding([.bottom, .top], 5)
+                    
                     ForEach(0..<recipe.directions.count, id: \.self) {index in
                         Text(String(index+1) + "•  " + recipe.directions[index])
                             .padding(.bottom, 5)
